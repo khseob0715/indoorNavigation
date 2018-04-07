9899dd349f4826e8c1792ee1a8e47e3f2b123a3d
Han Seob Kim <khseob0715@gmail.com>
2018-04-08 01:05:57 +0900

    Location Service

---
+++ app/src/main/java/com/example/aiden/indooratlas/GpsInfo.java
@@ -0,0 +1,180 @@
+package com.example.aiden.indooratlas;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.support.v4.content.ContextCompat;
+
+public class GpsInfo extends Service implements LocationListener{
+
+    private final Context mContext;
+    // 현재 GPS 사용유무
+    boolean isGPSEnabled = false;
+
+    // 네트워크 사용 유무
+    boolean isNetworkEnabled = false;
+
+    // GPS 상태 값
+    boolean isGetLocation = false;
+
+    Location location;
+    double lat, lon;
+
+    // 최소 GPS 정보 업데이트 거리 10 미터
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
+    // 최소 GPS 정보 업데이트 시간, 밀리세컨드이므로 1분.
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
+
+    protected LocationManager locationManager;
+    public GpsInfo(Context mContext) {
+        this.mContext = mContext;
+        getLocation();
+    }
+
+    public Location getLocation() {
+        if (Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission(
+                        mContext, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(
+                        mContext, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            return null;
+        }
+        try {
+            locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+
+            //GPS 정보 가져오기
+            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            //현재 네트워크 상태 값 알아오기
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if(!isGPSEnabled && !isNetworkEnabled) {
+                // GPS와 네트워크 사용이 가능하지 않을때
+            } else{
+                this.isGetLocation = true;
+                // 네트워크 정보로 부터 위치 값 가져오기
+                if(isNetworkEnabled){
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+
+                    if(locationManager != null){
+                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        // 위도 경도 저장
+                        lat = location.getLatitude();
+                        lon = location.getLongitude();
+                    }
+                }
+                if(isGPSEnabled){
+                    if(location == null){
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES,MIN_DISTANCE_CHANGE_FOR_UPDATES,this);
+                        if(locationManager != null){
+                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if(location != null){
+                                lat = location.getLatitude();
+                                lon = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return location;
+    }
+
+    // GPS 종료
+    public void stopUsingGPS(){
+        if(locationManager != null){
+            locationManager.removeUpdates(GpsInfo.this);
+        }
+    }
+
+    // 위도 값을 가져온다.
+    public double getLatitude(){
+        if(location != null){
+            lat = location.getLatitude();
+        }
+        return lat;
+    }
+
+    // 경도 값을 가져온다.
+    public double getLongitude(){
+        if(location != null){
+            lon = location.getLongitude();
+        }
+        return lon;
+    }
+
+    // GPS나 wifi 정보가 켜져 있는지 확인
+    public boolean isGetLocation(){
+        return this.isGetLocation;
+    }
+
+    // GPS 정보를 가져오지 못했을 때 설정값으로 갈지 물어보는 alert 창
+    public void showSettingAlert(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+
+        alertDialog.setTitle("GPS 사용유무셋팅");
+        alertDialog.setMessage("GPS 셋팅이 되지 않았을 수도 있습니다. \n 설정창으로 가시겠습니까?");
+
+        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                mContext.startActivity(intent);
+            }
+        });
+
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        alertDialog.show();
+    }
+
+
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
--- app/src/main/java/com/example/aiden/indooratlas/MainActivity.java
+++ app/src/main/java/com/example/aiden/indooratlas/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.aiden.indooratlas;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,23 +23,44 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private GoogleMap mMap;
     private MapView mapView = null;
 
+    private final int PERMISSIONS_ACCESS_FINE_LOCATION = 1000;
+    private final int PERMISSIONS_ACCESS_COARSE_LOCATION = 1001;
+    private boolean isAccessFineLocation = false;
+    private boolean isAccessCoarseLocation = false;
+    private boolean isPermission = false;
+
+    private GpsInfo gps;
+
+    private double latitude, longitude;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//
-//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-//                .findFragmentById(R.id.mapView);
-//        mapFragment.getMapAsync(this);
+
+        if(!isPermission){
+            callPermission();
+        }
+        gps = new GpsInfo(MainActivity.this);
+
+        if(gps.isGetLocation()){
+            latitude = gps.getLatitude();
+            longitude = gps.getLongitude();
+        }else {
+            gps.showSettingAlert();
+        }
+
         mapView = (MapView)findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
+
+        Log.e("tab","sssss");
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.e("tab","ssssssssssss");
         mapView.onResume();
     }
 
@@ -41,8 +69,43 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney = new LatLng(latitude, longitude);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 17));
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == PERMISSIONS_ACCESS_COARSE_LOCATION
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            isAccessFineLocation = true;
+        }else if(requestCode == PERMISSIONS_ACCESS_COARSE_LOCATION
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            isAccessCoarseLocation = true;
+        }
+
+        if(isAccessFineLocation && isAccessCoarseLocation){
+            isPermission = true;
+        }
+    }
+
+    private void callPermission(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_ACCESS_FINE_LOCATION);
+        }else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    PERMISSIONS_ACCESS_COARSE_LOCATION);
+        }else{
+            isPermission = true;
+        }
+    }
+
 }
